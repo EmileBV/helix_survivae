fc1687a6c14702c86c00fa97d41d7d4d55fbef03
EmileBV <emile.bv@gmail.com>
2021-04-11 11:30:32 -0400

    added savefile and fixed some coord stuff

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import curses
 import time
 from typing import Final
 from math import ceil, floor
+import json
 
 # entity IDs
 WALL: Final = 30
@@ -14,6 +15,7 @@ DOOR: Final = 10
 PLAYER: Final = 9
 TRAP_COLLECT: Final = 11
 TRAP_SET: Final = 21
+TRAP_PLACED: Final = 22
 
 OFFSET_START_X: Final = 0
 OFFSET_START_Y: Final = 1
@@ -25,6 +27,9 @@ GAME_TIME = 1.0 / 32.0
 FLASHER_MAX: Final = 10
 
 HEALTH_MAX: Final = 100
+HEALTH_BAR_SIZE: Final = 20
+
+TRAP_DAMAGE = 20
 
 
 def clamp(val, min_val, max_val):
@@ -66,6 +71,8 @@ def setup_colors():
     curses.init_pair(5, 8, curses.COLOR_BLACK)
     curses.init_pair(6, 9, curses.COLOR_BLACK)
     curses.init_pair(7, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(8, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(9, curses.COLOR_BLUE, curses.COLOR_BLACK)
 
 
 def get_color_pair_id(obj_id):
@@ -100,12 +107,24 @@ def draw_menu(stdscr):
         DOOR: "D",
         WALL: "W",
         TRAP_COLLECT: "x",
-        TRAP_SET: "X"
+        TRAP_SET: "X",
+        TRAP_PLACED: "X"
     }
     k = 0
     cursor_x = 0
     cursor_y = 0
+    player_x = 0
+    player_y = 0
     tiles = [[0 for i in range(10)] for j in range(10)]
+    health = HEALTH_MAX
+    score = 0
+    is_begin = True
+
+    if os.path.exists("save.json"):
+        with open("save.json", "r") as file:
+            (tiles, player_x, player_y, health, score) = json.loads(file.read())
+            is_begin = False
+
     last_dir = 0
 
     # Clear and refresh the screen for a blank canvas
@@ -121,8 +140,6 @@ def draw_menu(stdscr):
 
     flasher = FLASHER_MAX
 
-    health = HEALTH_MAX
-
     while k != ord('q'):
         start = time.perf_counter()
 
@@ -135,61 +152,82 @@ def draw_menu(stdscr):
             stdscr.clear()
             height, width = stdscr.getmaxyx()
 
-            t_h_max = height - OFFSET_START_Y - OFFSET_END_Y+1
+            t_h_max = height - OFFSET_START_Y - OFFSET_END_Y
             t_w_max = width - OFFSET_START_X - OFFSET_END_X
 
             tiles = resize(tiles, t_h_max, t_w_max)
 
-            target_x, target_y = cursor_x, cursor_y
-            if k == curses.KEY_DOWN:
-                target_y = cursor_y + 1
-            elif k == curses.KEY_UP:
-                target_y = cursor_y - 1
-            elif k == curses.KEY_RIGHT:
-                target_x = cursor_x + 1
-            elif k == curses.KEY_LEFT:
-                target_x = cursor_x - 1
+            if health <= 0:
+                health = HEALTH_MAX
+                score = 0
+                tiles = [[0 for i in range(t_h_max)] for j in range(t_w_max)]
+                is_begin = True
 
-            target_x = clamp(target_x, OFFSET_START_X, t_w_max-1)
-            target_y = clamp(target_y, OFFSET_START_Y, t_h_max-1)
+            if is_begin:
+                player_x = round(t_w_max/2)
+                player_y = round(t_h_max/2)
+                is_begin = False
+
+            target_x, target_y = player_x, player_y
+            if k == curses.KEY_DOWN:
+                target_y = player_y + 1
+            elif k == curses.KEY_UP:
+                target_y = player_y - 1
+            elif k == curses.KEY_RIGHT:
+                target_x = player_x + 1
+            elif k == curses.KEY_LEFT:
+                target_x = player_x - 1
+
+            target_x = clamp(target_x, 0, t_w_max-1)
+            target_y = clamp(target_y, 0, t_h_max-1)
 
             if tiles[target_x][target_y] < WALL:
-                cursor_x, cursor_y = target_x, target_y
+                player_x, player_y = target_x, target_y
+                cursor_x, cursor_y = player_x + OFFSET_START_X, player_y + OFFSET_START_Y
 
             if is_arrow(k):
                 last_dir = k
 
+            # building logic
             if k == ord('w'):
-                tiles[cursor_x][cursor_y] = WALL
+                tiles[player_x][player_y] = WALL
             elif k == ord('d'):
-                tiles[cursor_x][cursor_y] = DOOR
+                tiles[player_x][player_y] = DOOR
             elif k == ord('x'):
-                tiles[cursor_x][cursor_y] = TRAP_SET
+                tiles[player_x][player_y] = TRAP_PLACED
             elif k == ord(' '):
-                tiles[clamp(cursor_x + 1, OFFSET_START_X, t_w_max-1)][cursor_y] = 9
-                tiles[clamp(cursor_x - 1, OFFSET_START_X, t_w_max-1)][cursor_y] = 9
-                tiles[cursor_x][clamp(cursor_y + 1, OFFSET_START_Y, t_h_max-1)] = 9
-                tiles[cursor_x][clamp(cursor_y - 1, OFFSET_START_Y, t_h_max-1)] = 9
+                tiles[clamp(player_x + 1, OFFSET_START_X, t_w_max-1)][player_y] = 9
+                tiles[clamp(player_x - 1, OFFSET_START_X, t_w_max-1)][player_y] = 9
+                tiles[player_x][clamp(player_y + 1, OFFSET_START_Y, t_h_max-1)] = 9
+                tiles[player_x][clamp(player_y - 1, OFFSET_START_Y, t_h_max-1)] = 9
 
-            if 0 <= tiles[cursor_x][cursor_y] <= PLAYER:
-                tiles[cursor_x][cursor_y] = PLAYER
+            # damage logic
+            if 0 <= tiles[player_x][player_y] <= PLAYER:
+                tiles[player_x][player_y] = PLAYER
+            elif tiles[player_x][player_y] == TRAP_SET:
+                health -= TRAP_DAMAGE
+                tiles[player_x][player_y] = PLAYER
 
             # health and inventory eventually
-            player_info = f"HP: [{'#' * ceil(health/10)}{'-' * floor((HEALTH_MAX-health)/10)}] {health:03}"
-            stdscr.addstr(0, 1, player_info, curses.color_pair(1))
+            player_info = f"HP: [{'#' * ceil(health/HEALTH_MAX * HEALTH_BAR_SIZE)}{'-' * floor((HEALTH_MAX-health)/HEALTH_MAX * HEALTH_BAR_SIZE)}] {health:03}"
+            score_bar = f" | SCORE: {score:010}"
+            stdscr.addstr(0, 1, player_info, curses.color_pair(8))
+            stdscr.addstr(0, len(player_info) + 1, score_bar, curses.color_pair(9))
 
             # debug info maybe?
-            status_bar = f"Width: {width}, Height: {height}, pressed key: {k:03}, fps: {fps:.2f} (target is 30)"
+            status_bar = f"Width: {width}, Height: {height}, pressed key: {k:03}, fps: {fps:.2f} (target is 30), pos: x:{player_x}/y:{player_y}"
             stdscr.addstr(height - 1, 0, status_bar, curses.color_pair(1))
 
             # decrement
-            tiles = [[max(0, i - 1 if 0 < i <= PLAYER else i) for i in j] for j in tiles]
+            #tiles = [[max(0, i - 1 if 0 < i <= PLAYER else i) for i in j] for j in tiles]
 
             # draw map
-            for i in range(OFFSET_START_X, t_w_max):
-                for j in range(OFFSET_START_Y, t_h_max):
+            for i in range(0, t_w_max):
+                for j in range(0, t_h_max):
                     tile_id = tiles[i][j]
-                    stdscr.addstr(j, i, char_map[tile_id], curses.color_pair(get_color_pair_id(tile_id)))
+                    tiles[i][j] = tile_id - 1 if 0 < tile_id <= PLAYER or (tile_id == TRAP_PLACED and (i != player_x or j != player_y)) else tile_id
+                    tile_id = tiles[i][j]
+                    stdscr.addstr(j+OFFSET_START_Y, i+OFFSET_START_X, char_map[tile_id], curses.color_pair(get_color_pair_id(tile_id)))
 
             if flasher > 2:
                 stdscr.addstr(cursor_y, cursor_x, char_map[PLAYER], curses.color_pair(get_color_pair_id(PLAYER)))
@@ -207,6 +245,9 @@ def draw_menu(stdscr):
         frame_time = time.perf_counter() - start
         time_acc += frame_time
 
+    with open("save.json", "w") as file:
+        file.write(json.dumps((tiles, player_x, player_y, health, score)))
+
 
 def main():
     curses.wrapper(draw_menu)
@@ -214,6 +255,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    # tiles = [[0 for i in range(10)] for j in range(10)]
-    # resize(tiles, 30, 50)
-    # resize(tiles, 20, 10)
